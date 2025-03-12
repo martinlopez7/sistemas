b7cc09b0327e30634dfd333115b16d49dae12bd1
martin <martin.lopez@alumnos.uneatlantico.es>
2025-03-12 17:54:32 +0100

    avance

--- src/11caracterRepetido.c
+++ src/11caracterRepetido.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char palabra[50];
-    int caracteres[256] = {0};
+    char palabra[100];
+    int i, j;
     
     printf("Ingresa una palabra: ");
     scanf("%s", palabra);
     
-    for (int i = 0; i < strlen(palabra); i++) {
-        if (caracteres[(unsigned char)palabra[i]]) { 
-            printf("Hay caracteres repetidos.\n");
-            return 0;
+    for (i = 0; palabra[i] != '\0'; i++) {
+        for (j = i + 1; palabra[j] != '\0'; j++) {
+            if (palabra[i] == palabra[j]) {
+                printf("Hay caracteres repetidos: '%c'\n", palabra[i]);
+                return 0;
+            }
         }
-        caracteres[(unsigned char)palabra[i]] = 1;
     }
     
     printf("No hay caracteres repetidos.\n");
